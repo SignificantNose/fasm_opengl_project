083196295bc0743bb22c59b70bbaabe971cd3c7c
SignificantNose <yanucevich.d@gmail.com>
2023-11-25 14:52:21 +0300

    mesh structs modified significantly. tower generation in shaders implemented

--- D_code/BUILD_code.asm
+++ D_code/BUILD_code.asm
@@ -5,15 +5,16 @@ aRadian   dd    57.3
 aThousand dw    1000
 anEleven  dw    11
 aNinety   dw    90
-startValue Vector3
-StartHeight     Vector3      0.0,0.0,0.0
-TempMesh        PackedMesh   0,0,0,0
 
 
-proc    Build.GeneratePackedTower uses ebx edi, resultMesh, startCoordsSrc, scale
+; routine that generates a PackedMesh struct of
+; a tower with the radius of the basis of the 
+; building equal to scale.
+; the amount of the floors is generated randomly
+proc    Build.GeneratePackedTower uses ebx edi, pPackedMesh, scale
         locals
                 numOfFloorsLiterally    dd      ?
-                currHeight              dd      ?
+                currHeight              dd      0.0
                 cosValue                dd      ?
                 sinValue                dd      ?
                 sinValueNeg             dd      ?
@@ -22,221 +23,230 @@ proc    Build.GeneratePackedTower uses ebx edi, resultMesh, startCoordsSrc, scal
                 temp                    dd      ?
         endl
 
-        mov eax, [startCoordsSrc]
-        push eax
-        push startValue
-        stdcall Vector3.Copy
-        fld     [startValue.y]
-        fstp    [currHeight]
+        ; copying start coords to own variable
+        ; (it's probably not a good idea to use them)
+        ; (when the generation of town is executed, 
+        ; their transformation coords must be calculated)
+        ; mov eax, [startCoordsSrc]
+        ; push eax
+        ; push startValue
+        ; stdcall Vector3.Copy
+        ; fld     [startValue.y]
+        ; fstp    [currHeight]
 
 ; generating the amount of floors; literal floors, not walls: walls = numOfFloorsLiterally-1
-        stdcall Rand.GetRandomNumber, 2, 10                 ; generating the amount of floors
-        mov     [numOfFloorsLiterally], eax     ; i can just push it?
+        stdcall   Rand.GetRandomInBetween, 2, 10                 ; generating the amount of floors
+        mov       [numOfFloorsLiterally], eax     ; i can just push it?
 
 ; allocating the memory and saving the handle
-        xor edx, edx
-        mov edi, 4*sizeof.Vertex
-        mul edi                         ; eax = eax * 4 * 4 * 3
-        push eax
-        invoke HeapAlloc, [hHeap], 8
-        mov edx, [resultMesh]
-        mov [edx+PackedMesh.vertices], eax
-        mov edi, eax
+        xor     edx, edx
+        mov     edi, 4*sizeof.Vertex
+        mul     edi                         ; eax = eax * sizeof(oneFloor)
+
+        push    eax
+        invoke  HeapAlloc, [hHeap], 8
+
+        ; the result must be a pointer, so the result mesh
+        ; will not be a struct, but a pointer to a struct
+        mov     edi, [pPackedMesh]
+        mov     [edi + PackedVerticesMesh.pVertices], eax
+        mov     edi, eax
 
 ; generating vertices
-        mov ecx, [numOfFloorsLiterally]
+        mov     ecx, [numOfFloorsLiterally]
 .GenVertices:
-        push ecx
+        push    ecx
 
 ;gen sin, cos
         stdcall Rand.GetRandomNumber, ebx, 10000
-        fild [aNinety]       ; 90
-        fild [RandomValue]     ; [0;10000], 90
-        fidiv [aThousand]    ; [0;10], 90
-        fchs                 ; -x, 90
-        fiadd [anEleven]     ; -x+11, 90
-        fdivp                ; 90/(-x+11)
-        fdiv  [aRadian]      ;angleRad
+        fild    [aNinety]               ; 90
+        fild    [RandomValue]           ; [0;10000], 90
+        fidiv   [aThousand]             ; [0;10], 90
+        fchs                            ; -x, 90
+        fiadd   [anEleven]              ; -x+11, 90
+        fdivp                           ; 90/(-x+11)
+        fdiv    [aRadian]               ; angleRad
 
-        fsincos        ; sin, cos
-        fmul [scale]   ; R*sin, cos
-        fld  st0       ; R*sin, R*sin, cos
-        fchs           ; -R*sin, R*sin, cos
-        fstp [sinValueNeg]   ; R*sin, cos
-        fstp [sinValue]      ; cos
-        fmul [scale]   ; R*cos
-        fld  st0       ; R*cos, R*cos
-        fchs           ; -R*cos, R*cos
-        fstp [cosValueNeg]   ; R*cos
-        fstp [cosValue]      ;
+        fsincos                         ; sin, cos
+        fmul    [scale]                 ; R*sin, cos
+        fld     st0                     ; R*sin, R*sin, cos
+        fchs                            ; -R*sin, R*sin, cos
+        fstp    [sinValueNeg]           ; R*sin, cos
+        fstp    [sinValue]              ; cos
+        fmul    [scale]                 ; R*cos
+        fld     st0                     ; R*cos, R*cos
+        fchs                            ; -R*cos, R*cos
+        fstp    [cosValueNeg]           ; R*cos
+        fstp    [cosValue]              ;
 
 
 ;gen vertices themselves
 
-        ;mov eax, [cosValue]
+
+        mov     eax, [cosValue]
+        stosd
+        mov     eax, [currHeight]
+        stosd
+        mov     eax, [sinValue]
+        stosd
+        ;fld [cosValue]
+        ;fadd [startValue.x]
+        ;fstp [temp]
+        ;mov eax, [temp]
         ;stosd
         ;mov eax, [currHeight]
         ;stosd
-        ;mov eax, [sinValue]
+        ;fld [sinValue]
+        ;fadd [startValue.z]
+        ;fstp [temp]
+        ;mov eax, [temp]
         ;stosd
-        fld [cosValue]
-        fadd [startValue.x]
-        fstp [temp]
-        mov eax, [temp]
-        stosd
-        mov eax, [currHeight]
-        stosd
-        fld [sinValue]
-        fadd [startValue.z]
-        fstp [temp]
-        mov eax, [temp]
-        stosd
 
 
 
-        jmp @F
+        jmp     @F
 .GenVerticesContinue:
-        jmp .GenVertices
+        jmp     .GenVertices
 @@:
 
-        ;mov   eax, [sinValueNeg]
+        mov     eax, [sinValueNeg]
+        stosd
+        mov     eax, [currHeight]
+        stosd
+        mov     eax, [cosValue]
+        stosd
+
+        ;fld [sinValueNeg]
+        ;fadd [startValue.x]
+        ;fstp [temp]
+        ;mov eax, [temp]
         ;stosd
         ;mov eax, [currHeight]
         ;stosd
-        ;mov   eax, [cosValue]
+        ;fld [cosValue]
+        ;fadd [startValue.z]
+        ;fstp [temp]
+        ;mov eax, [temp]
         ;stosd
-        fld [sinValueNeg]
-        fadd [startValue.x]
-        fstp [temp]
-        mov eax, [temp]
+
+        mov     eax, [cosValueNeg]
         stosd
-        mov eax, [currHeight]
+        mov     eax, [currHeight]
         stosd
-        fld [cosValue]
-        fadd [startValue.z]
-        fstp [temp]
-        mov eax, [temp]
+        mov     eax, [sinValueNeg]
         stosd
 
-        ;mov  eax, [cosValueNeg]
+        ;fld [cosValueNeg]
+        ;fadd [startValue.x]
+        ;fstp [temp]
+        ;mov eax, [temp]
         ;stosd
         ;mov eax, [currHeight]
         ;stosd
-        ;mov  eax, [sinValueNeg]
+        ;fld [sinValueNeg]
+        ;fadd [startValue.z]
+        ;fstp [temp]
+        ;mov eax, [temp]
         ;stosd
-        fld [cosValueNeg]
-        fadd [startValue.x]
-        fstp [temp]
-        mov eax, [temp]
-        stosd
-        mov eax, [currHeight]
-        stosd
-        fld [sinValueNeg]
-        fadd [startValue.z]
-        fstp [temp]
-        mov eax, [temp]
-        stosd
 
 
 
-        ;mov  eax, [sinValue]
+        mov     eax, [sinValue]
+        stosd
+        mov     eax, [currHeight]
+        stosd
+        mov     eax, [cosValueNeg]
+        stosd
+
+        ;fld [sinValue]
+        ;fadd [startValue.x]
+        ;fstp [temp]
+        ;mov eax, [temp]
         ;stosd
         ;mov eax, [currHeight]
         ;stosd
-        ;mov  eax, [cosValueNeg]
+        ;fld [cosValueNeg]
+        ;fadd [startValue.z]
+        ;fstp [temp]
+        ;mov eax, [temp]
         ;stosd
-        fld [sinValue]
-        fadd [startValue.x]
-        fstp [temp]
-        mov eax, [temp]
-        stosd
-        mov eax, [currHeight]
-        stosd
-        fld [cosValueNeg]
-        fadd [startValue.z]
-        fstp [temp]
-        mov eax, [temp]
-        stosd
 
 
         fld     [currHeight]
         fadd    [scale]
-        fstp    [currHeight]
+        fstp    [currHeight]  
 
-        pop ecx
-        loop .GenVerticesContinue
+        pop     ecx
+        loop    .GenVerticesContinue
 
 ; generating indices
-        xor edx, edx
-        mov eax, [numOfFloorsLiterally]         ; = n
-        mov ecx, 6*4                            ; 6 = num of bytes for one wall, 4 = num of walls per *floor*
-        mul ecx
-        sub eax, 6*2                            ; adjustment for walls (didn't decrease eax at first, needed to be corrected)
+        mov     eax, [numOfFloorsLiterally]
+        shl     eax, 2
+        sub     eax, 2                              ; eax = 4n-2 = №of rectangles
 
-        push eax
-        invoke HeapAlloc, [hHeap], 8            ; allocates enough memory for outer walls and the floors at the top and at the bottom
-        mov edx, [resultMesh]
-        mov [edx+PackedMesh.indices], eax
-        mov edi, eax
+        shl     eax, 1                              ; eax = №of triangles
+        mov     edi, [pPackedMesh]
+        mov     [edi+PackedVerticesMesh.trianglesCount], eax
 
 
-        xor eax, eax
+        xor     edx, edx
+        mov     ecx, 3                              ; 3 = №of vertices per triangle
+        mul     ecx 
+        
+
+        push    eax
+        invoke  HeapAlloc, [hHeap], 8               ; allocates enough memory for outer walls and the floors at the top and at the bottom
+        mov     [edi + PackedVerticesMesh.pIndices], eax
+        mov     edi, eax
+
+        xor     eax, eax
 
         stdcall GenFloorCeil
 
 
-        mov eax, [numOfFloorsLiterally]
-        dec eax
-        mov ecx, eax                            ; the amount of walls i need to draw
-        shl eax, 3
-        add eax, 4
-        mov [edx+PackedMesh.trianglesCount], eax
+        mov     eax, [numOfFloorsLiterally]
+        dec     eax
+        mov     ecx, eax                            ; the amount of walls i need to draw = n - 1
 
-        xor eax, eax
+        xor     eax, eax
 
 
 .GenWalls:
-        push ecx
-        mov ecx, 3
+        push    ecx
+        mov     ecx, 3
 .GenSomeWalls:
         stosb                      ; these parts of code look the same
-        add al, 4
+        add     al, 4
         stosb
-        inc al
+        inc     al
         stosb
 
         stosb                      ; these parts of code look the same
-        sub al, 4
+        sub     al , 4
         stosb
-        dec al
+        dec     al 
         stosb
-        inc al
+        inc     al 
         loop .GenSomeWalls
 
         stosb                       ; these parts of code look the same
-        add al, 4
+        add     al, 4
         stosb
-        sub al, 3
+        sub     al, 3
         stosb
 
         stosb                      ; these parts of code look the same
-        sub al, 4
+        sub     al, 4
         stosb
-        add al, 3
+        add     al, 3
         stosb
-        inc al
+        inc     al
 
-        pop ecx
+        pop     ecx
         loop .GenWalls
 
         stdcall GenFloorCeil
 
-        mov eax, [cubeMesh.colors]  ; what the hell is this ; delete after shaders done
-
-        mov [edx+PackedMesh.colors], eax
-
-        ;mov eax, [theMesh]
-        ;mov [resultMesh], eax
         ret
 
 endp
@@ -257,6 +267,59 @@ proc    GenFloorCeil
         stosb
         sub al, 3
         stosb
+
+        ;stosd 
+        ;inc     eax 
+        ;stosd 
+        ;inc     eax 
+        ;stosd 
+        ;stosd 
+        ;inc     eax 
+        ;stosd
+        ;sub     eax, 3
+        ;stosd
+
+        ret
+endp
+
+
+; routine responsible for applying a texture
+; to the tower to make it look like it's 
+; composed of triangles
+proc Build.DesignTower uses edi,\
+        pTowerMesh
+
+        mov     edi, [pTowerMesh]
+        mov     eax, [edi + Mesh.VerticesCount]
+        mov     edi, [edi + Mesh.pTexCoords]
+        
+        ; acquiring the amount of triangles
+        xor     edx, edx
+        mov     ecx, 3          
+        div     ecx 
+        xchg    ecx, eax 
+
+        ; generating the texture coordinate themselves
+.looper:
+        ;mov     eax, 0.0
+        xor     eax, eax
+        stosd 
+        ;mov     eax, 0.0
+        stosd 
+
+        mov     eax, 0.5
+        stosd 
+        mov     eax, 1.0
+        stosd 
+
+        mov     eax, 1.0
+        stosd 
+        ;mov     eax, 0.0
+        xor     eax, eax
+        stosd 
+
+        loop    .looper
+
 
         ret
 endp
@@ -334,5 +397,4 @@ proc Build.GenerateTown uses ebx edi, width, height, scale, resultTown
 
         ret
 endp
-
 
--- fragment.glsl
+++ fragment.glsl
@@ -3,15 +3,11 @@
 
 out vec4 FragClr;
 
-in vec3 ourColor;
 in vec2 TexCoord;
 uniform sampler2D ourTexture;
 
-//in vec4 vertexColor;
-//uniform vec4 outColor;
 
 void main(void)
 {
-        FragClr = texture(ourTexture, TexCoord);//*vec4(ourColor,1.0);
-        //FragClr = vec4(ourColor,1.0);
+        FragClr = texture(ourTexture, TexCoord);
 }
